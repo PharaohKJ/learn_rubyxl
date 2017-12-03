6ea7d01e9d53ba07677557237321a8e55211bfe7
PharaohKJ <kato@phalanxware.com>
2017-12-03 12:41:39 +0900

    1st commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Ruby で xlsx を読み書きしたい
+
+いろいろ調べたけど Windows OLE を使わない限り  rubyxl がベストなようだ。
+
+[RubyでExcelファイルを扱うライブラリの比較 - Qiita](https://qiita.com/damassima/items/1b791ba90459ef0534fe)
+
+
+http://secret-garden.hatenablog.com/entry/2017/09/21/174348 上記ページよりコードを拝借していろいろ実験してみた。
+
+# 実行方法
+
+``` shell-session
+bundle install
+bundle exec ruby main.rb
+```
---
+++ main.rb
@@ -0,0 +1,45 @@
+# coding: utf-8
+require "rubyXL"
+
+# original from http://secret-garden.hatenablog.com/entry/2017/09/21/174348
+
+src  = "test.xlsx"
+dest = "output.xlsx"
+
+# エクセルファイルの読み込み
+book = RubyXL::Parser.parse(src)
+
+# シートを取得
+sheet = book[0]
+
+# シートのダンプ1
+
+sheet.each { |row|
+   row && row.cells.each { |cell|
+     val = cell && cell.value
+     p val
+   }
+}
+
+# index指定シート
+(0..2).each do |x|
+  (0..2).each do |y|
+    p sheet[x][y]
+  end
+end
+
+
+# シートのセルを結合する
+
+# 行3, 列 0 のセル位置
+cell1 = [3, 0]
+# 行3, 列 1 のセル位置
+cell2 = [3, 1]
+
+# cell1 と cell2 を結合する
+sheet.merge_cells *cell1, *cell2
+# 以下と等価
+# sheet.merge_cells 3, 0, 3, 1
+
+# ファイルの保存
+book.write(dest)
