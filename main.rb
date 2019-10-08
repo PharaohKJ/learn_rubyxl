9b60e71e05fdc7e550da770613d1aef5fc9cd358
PharaohKJ <kato@phalanxware.com>
2019-10-08 22:20:59 +0900

    CVE対応と表示をわかりやすく

--- main.rb
+++ main.rb
@@ -2,13 +2,50 @@
 require 'rubyXL'
 require 'rubyXL/convenience_methods'
 
+def convert_alphabet_to_col_num(alphabet)
+  ret = 0
+  size = alphabet.size
+  alphabet.to_s.each_char do |char|
+    ret += (char.ord - 'A'.ord + 1) * 26 ** (size - 1)
+    size -= 1
+  end
+  ret
+end
+
+p convert_alphabet_to_col_num('XEC')
+
+return
+
+
+book = RubyXL::Parser.parse('t2.xlsx')
+
 # original from http://secret-garden.hatenablog.com/entry/2017/09/21/174348
+
+book.each do |sheet|
+  sheet.each { |row|
+    row && row.cells.each_with_index { |cell, i|
+      val = cell && cell.value
+     if val.nil?
+       print "#{i}[nil]"
+     else
+       p val
+     end
+    }
+  }
+end
+
+return
+
 
 src  = "test.xlsx"
 dest = "output.xlsx"
 
 # エクセルファイルの読み込み
 book = RubyXL::Parser.parse(src)
+
+
+
+
 
 # シートを取得
 sheet = book[0]
